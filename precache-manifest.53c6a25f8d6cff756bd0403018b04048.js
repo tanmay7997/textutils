002758270daccf4a305944a745212b55e837874a
Tanmay Salvi <salvitanmay07@gmail.com>
2022-05-29 13:37:59 +0530

    Updates

---
+++ precache-manifest.53c6a25f8d6cff756bd0403018b04048.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a49c5e61e72100dd1bb",
+    "url": "/textutils/static/css/main.0cb05e94.chunk.css"
+  },
+  {
+    "revision": "7a49c5e61e72100dd1bb",
+    "url": "/textutils/static/js/main.7a49c5e6.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/textutils/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/textutils/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "62145ba569f44a4585e3",
+    "url": "/textutils/static/js/runtime~main.62145ba5.js"
+  },
+  {
+    "revision": "53bbe3c1a7203d71d920ff6028069772",
+    "url": "/textutils/index.html"
+  }
+];
